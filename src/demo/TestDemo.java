ff86af093087005f77a51259a2fe8bbac5e970c2
Samuel <Samuel@192.168.0.21>
2024-01-05 16:05:19 -0800

    initial commit

---
+++ src/demo/TestDemo.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class TestDemo {
+	public static void main(String[] args) {
+		System.out.println("Today class is stash ");
+		
+		
+		System.out.println("weteseney edris ali ");
+	}
+
+}
